302cbbe8fd178f4a274eaf00be41c0d721f2a794
YeisonKirax <yeison.fernandez.14@sansano.usm.cl>
2018-09-25 22:20:32 -0300

    Add producto-api
    Work with an initial table "productos" with a id, name, date_created, date_modified fields.

---
+++ producto/instance/config.py
@@ -0,0 +1,31 @@
+import os
+
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = "SUPERCARIFLAGILISTICOESPIRALIDOSO"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/producto"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/test_db"
+
+class StagingConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'staging': StagingConfig,
+        'production': ProductionConfig,
+}
